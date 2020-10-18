f908f3c3baad14c5d248f4818176d73d0b984c2e
Duane Deane <lilboom@Duanes-Air.attlocal.net>
2020-10-18 14:50:11 -0500

    trying to fix delete

--- wwwroot/js/catalog.js
+++ wwwroot/js/catalog.js
@@ -1,26 +1,25 @@
-
-function fetchData(){
-    $.ajax({
-        url: "/catalog/GetProperties",
-        type: "GET",
-        success: (data) => {
-            console.log(data);
-            for(let i=0; i< data.length; i++){
-                displayProperty(data[i]);
-            }
-        },
-        error: (details) => {
-            console.log("Error, details");
-        },
-    });
+function fetchData() {
+  $.ajax({
+    url: "/catalog/GetProperties",
+    type: "GET",
+    success: (data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        displayProperty(data[i]);
+      }
+    },
+    error: (details) => {
+      console.log("Error, details");
+    },
+  });
 }
 
-function displayProperty(property){
-    const bedIcon='<i class="fas fa-bed"></i>';
-    const bathIcon='<i class="fas fa-bath"></i>';
-    var syntax = `<div class='property'>
+function displayProperty(property) {
+  const bedIcon = '<i class="fas fa-bed"></i>';
+  const bathIcon = '<i class="fas fa-bath"></i>';
+  var syntax = `<div class='property'>
         <img src='${property.picture}'>
-        <p>${property.price}</p>
+        <p>$${property.price}</p>
         <h5>${property.address}</h5>
         <h6>${property.size}</h6>
         <h6>${bedIcon}${property.beds}</h6>
@@ -29,28 +28,28 @@ function displayProperty(property){
         <h6>${property.parking}</h6>
     </div>`;
 
-    var container = $("#catalog-container");
+  var container = $("#catalog-container");
 
-    container.append(syntax);
+  container.append(syntax);
 }
 
-function init(){
-    fetchData();
+function init() {
+  fetchData();
 }
 
 function testDelete() {
-    var id = 1;
-    $.ajax({
-      url: "/catalog/DeleteProperty/" + id,
-      type: "DELETE",
-      success: () => {
-        console.log("deleted");
-      },
-      error: (details) => {
-        console.log("Error", details);
-      },
-    });
-  }
+  var id = 4;
+  $.ajax({
+    url: "/catalog/DeleteProperty/" + id,
+    type: "DELETE",
+    success: () => {
+      console.log("deleted");
+    },
+    error: (details) => {
+      console.log("Error", details);
+    },
+  });
+}
 
 window.onload = init;
 
--- wwwroot/js/register.js
+++ wwwroot/js/register.js
@@ -1,72 +1,92 @@
 var UI = {};
 
-function Property(address,picture,size,beds,baths,parking,price,description){
-    this.address = address;
-    this.beds = beds;
-    this.baths = baths;
-    this.price = price;
-    this.size = size;
-    this.picture = picture;
-    this.description = description;
-    this.parking = parking;
-  }
+function Property(
+  address,
+  picture,
+  size,
+  beds,
+  baths,
+  parking,
+  price,
+  description
+) {
+  this.address = address;
+  this.beds = beds;
+  this.baths = baths;
+  this.price = price;
+  this.size = size;
+  this.picture = picture;
+  this.description = description;
+  this.parking = parking;
+}
 
 function saveProperty() {
-    var address = UI.Address.val();
-    var picture = UI.Picture.val();
-    var description = UI.Description.val();
-    var size = parseInt(UI.Size.val());
-    var beds = parseInt(UI.Beds.val());
-    var baths = parseInt(UI.Baths.val());
-    var parking = UI.Parking.val() == "yes";
-    var price = parseFloat(UI.Price.val());
+  var address = UI.Address.val();
+  var picture = UI.Picture.val();
+  var description = UI.Description.val();
+  var size = parseInt(UI.Size.val());
+  var beds = parseInt(UI.Beds.val());
+  var baths = parseInt(UI.Baths.val());
+  var parking = UI.Parking.val() == "yes";
+  var price = parseFloat(UI.Price.val());
 
-    if(!price || !size) {
-        $("#alertError").removeClass('hide');
+  if (!price || !size) {
+    $("#alertError").removeClass("hide");
 
-        setTimeout(() => {$("#alertError").addClass('hide')},3000);
-    }
+    setTimeout(() => {
+      $("#alertError").addClass("hide");
+    }, 3000);
+  }
 
-    var property = new Property(address,picture,size,beds,baths,parking,price,description)
-    console.log(property);
+  var property = new Property(
+    address,
+    picture,
+    size,
+    beds,
+    baths,
+    parking,
+    price,
+    description
+  );
+  console.log(property);
 
-   $.ajax({
-       url: "/catalog/SaveProperty",
-       type: "POST",
-       data: JSON.stringify(property),
-       contentType: "application/json",
-       success: function(res) {
-           console.log(res);
-           
-           $("#alertSuccess").removeClass('hide');
+  $.ajax({
+    url: "/catalog/SaveProperty",
+    type: "POST",
+    data: JSON.stringify(property),
+    contentType: "application/json",
+    success: function (res) {
+      console.log(res);
 
-            setTimeout(function(res){
-                $("#alertSuccess").addClass('hide');
-            },3000);
+      $("#alertSuccess").removeClass("hide");
 
-           $(".form-control").val('');
-       },
-       error: function(details) {
-            console.log("Error", details);
-       }
-   });
+      setTimeout(function (res) {
+        $("#alertSuccess").addClass("hide");
+      }, 3000);
+
+      $(".form-control").val("");
+    },
+    error: function (details) {
+      console.log("Error", details);
+    },
+  });
 }
 
 function init() {
-    console.log("register page");
+  console.log("register page");
 
-    $("#btnSave").click(saveProperty);
+  $("#btnSave").click(saveProperty);
 
-    UI = {
-        Address: $("#Address"),
-        Picture: $("#Picture"),
-        Description: $("#Description"),
-        Size: $("#Size"),
-        Beds: $("#Beds"),
-        Baths: $("#Baths"),
-        Parking: $("#Parking"),
-        Price: $("#Price")
-    };
+  UI = {
+    Address: $("#Address"),
+    Picture: $("#Picture"),
+    Description: $("#Description"),
+    Size: $("#Size"),
+    Beds: $("#Beds"),
+    Baths: $("#Baths"),
+    Parking: $("#Parking"),
+    Price: $("#Price"),
+  };
 }
 
 window.onload = init;
